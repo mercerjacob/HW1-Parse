df8b248f776724fba7585b2ce6aff636c03d67f6
Jake Mercer <mercerwork77@gmail.com>
2025-01-13 08:22:11 -0800

    added all

---
+++ seqparser/seq.py
@@ -0,0 +1,22 @@
+# DNA -> RNA Transcription
+from typing import Union
+
+TRANSCRIPTION_MAPPING = {"A": "U", "C": "G", "T": "A", "G": "C"}
+ALLOWED_NUC = TRANSCRIPTION_MAPPING.keys()
+
+
+def transcribe(seq: str, reverse: bool = False) -> str:
+    """
+    Write a function that will transcribe (replace DNA sequence to RNA
+    by replacing all 'T' to 'U') in an input sequence
+    """
+    pass
+
+def reverse_transcribe(seq: str) -> str:
+    """
+    Write a function that will transcribe an input sequence and reverse
+    the sequence
+    """
+    # Hey this is my comment
+    # Again!
+    pass
---
+++ tests/test_parsers.py
@@ -0,0 +1,60 @@
+# write tests for parsers
+
+from seqparser import (
+        FastaParser,
+        FastqParser)
+
+import pytest
+
+
+def test_freebie_parser_1():
+    """
+    This one is a freebie
+    DO NOT MODIFY THIS FUNCTION
+    """
+    assert True # things after the assert are true statements
+
+
+def test_freebie_parser_2():
+    """
+    This too is a freebie
+    DO NOT MODIFY THIS FUNCTION
+    """
+    assert 1 != 2
+
+        
+def test_FastaParser():
+    """
+    Write your unit test for your FastaParser class here. You should generate
+    an instance of your FastaParser class and assert that it properly reads in
+    the example Fasta File.
+
+    Some example of "good" test cases might be handling edge cases, like Fasta
+    files that are blank or corrupted in some way. Two example Fasta files are
+    provided in /tests/bad.fa and /tests/empty.fa
+    """
+    pass
+
+
+def test_FastaFormat():
+    """
+    Test to make sure that a fasta file is being read in if a fastq file is
+    read, the first item is None
+    """
+    pass
+
+
+def test_FastqParser():
+    """
+    Write your unit test for your FastqParser class here. You should generate
+    an instance of your FastqParser class and assert that it properly reads 
+    in the example Fastq File.
+    """
+    pass
+
+def test_FastqFormat():
+    """
+    Test to make sure fastq file is being read in. If this is a fasta file, the
+    first line is None
+    """
+    pass
